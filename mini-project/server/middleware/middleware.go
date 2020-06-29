2527bd4272e63c751e77b8fd4558736d4f43379e
kpaturi <kpaturi@ipromoteu.com>
2020-06-29 15:09:57 +0530

    Added mongodb referects

--- mini-project/server/middleware/middleware.go
+++ mini-project/server/middleware/middleware.go
@@ -1,7 +1,13 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func IndexPage(res http.ResponseWriter, req *http.Request) {
@@ -22,4 +28,21 @@ func EditTask(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("Edit"))
 }
 
+const connectionString = ""
+const databaseName = ""
 
+var collection *mongo.Collection
+
+const colelctionName = "todolist"
+
+func init() {
+	//Seeting the client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+	//conenct to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to Mongobd")
+	collection = client.Database(databaseName).Collection(colelctionName)
+}
