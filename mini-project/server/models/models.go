5b8b3721f1349d79233f89e542fdea106129017c
kpaturi <kpaturi@ipromoteu.com>
2020-06-29 14:48:08 +0530

    Fixed issues

--- mini-project/server/models/models.go
+++ mini-project/server/models/models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type ToDoList struct {
-	ID     primitive.ObjectID `json:_id,omitempty bson:"_id,omitempty"`
-	Task   string             `json: task,omitempty`
-	Status bool               `json: status,omitempty`
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task   string             `json: "task,omitempty"`
+	Status bool               `json: "status,omitempty"`
 }
